a71f97ef17cd794b3f7d2ca79c8be4d2da340eb1
Kristian Aurlien <kristian@aurlien.net>
2016-10-16 19:02:43 +0200

    Clean up, add comments and remove outliers

---
+++ 03 - Data from the Web/imports.py
@@ -0,0 +1,16 @@
+# We are going to use requests to do the HTTP-calls for gathering data, and BeautifulSoup for parsing the 
+# HTML that we recieve
+import requests
+from bs4 import BeautifulSoup
+
+# re will help us parse the html by using regular expressions
+import re
+
+# Furthermore, we will use the normal stack of pandas, numpy, matplotlib and seaborn
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as pls
+import seaborn as sns
+
+# Statistical test library
+import scipy.stats as stats
